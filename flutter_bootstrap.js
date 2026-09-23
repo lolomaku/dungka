8d64a84762947cd6db770bedd6226ab06d048fb4
Lolo Maku <markpajao@outlook.com>
2026-09-24 02:56:36 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2544567177" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1461071739" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
